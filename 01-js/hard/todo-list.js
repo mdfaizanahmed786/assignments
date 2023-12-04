f7008cdaa2c3b5504f8b3d5cca21e5d19e1d1221
Mohammed Faizan Ahmed <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-12-04 19:56:59 +0000

    feat: completed todo list class

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,41 @@
 
 class Todo {
 
+  constructor(todos=[]) {
+    this.todos = todos;
+  }
+  add(todo) {
+    this.todos.push(todo)
+  }
+  remove(indexOfTodo) {
+    if (indexOfTodo > -1) {
+      this.todos.splice(indexOfTodo, 1)
+    }
+  }
+
+  update(indexOfTodo, updatedTodo) {
+    let todosLength = this.todos.length;
+    for (let i = 0; i < todosLength; i++) {
+      if (indexOfTodo === i) {
+        this.todos[i] = updatedTodo;
+      }
+    }
+
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo<-1 || indexOfTodo >=this.todos.length) return null;
+    return this.todos[indexOfTodo];
+  }
+
+  clear(){
+    this.todos=[];
+  }
+
 }
 
 module.exports = Todo;
