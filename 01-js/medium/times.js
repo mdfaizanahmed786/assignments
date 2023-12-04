d0e61c151edab1878caf1c37b36dfdb7647dd548
Mohammed Faizan Ahmed <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-12-04 21:08:36 +0000

    feat: added this times feature

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,22 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateSumTime(n) {
+    const startTime=new Date().getTime();
+    let sum=0;
+
+    for(let i=0; i<=n; i++){
+        sum+=i;
+    }
+
+    const endTime=new Date().getTime();
+
+    let totalTime=(endTime-startTime)/1000;
+     
+    console.log(`The result is: ${sum}`)
+    console.log(`Total time taken is: ${totalTime}`)
 }
+
+calculateSumTime(100);
+calculateSumTime(100000);
+calculateSumTime(1000000000);
